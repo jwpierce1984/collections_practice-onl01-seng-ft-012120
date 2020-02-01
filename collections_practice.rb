ff18d47d3dfa9cbf5c937a3dec99aa412f388f22
Justin Pierce <jwpierce1984@gmail.com>
2020-02-01 02:56:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,6 +7,5 @@ def sort_array_desc (array)
 end
 
 def sort_array_char_count (array)
-  array.sort{|left, right|}
-  left.length <=> right.length
+  array.sort{|left, right| left.length <=> right.length}
 end
